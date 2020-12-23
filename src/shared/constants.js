95b033fda77bd7b7b7c435df117daf4ddffc2158
ouiigo <75907352+ouiigo@users.noreply.github.com>
2020-12-23 23:24:34 +0100

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,4 +1,4 @@
-export const token = 'bti26hf48v6uv69lirj0';
+export const token = 'bvhs2r748v6okueau4h0';
 
 export const getCandleUrl = 'https://finnhub.io/api/v1/stock/candle?';
 export const getBasicInfoUrl = 'https://finnhub.io/api/v1/stock/profile2?';
